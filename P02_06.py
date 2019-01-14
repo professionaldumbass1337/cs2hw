e19dfacfd5cf9555246072ea2d367ca86608ef99
안녕 세계 <35412512+professionaldumbass1337@users.noreply.github.com>
2019-01-14 17:43:59 +0700

    Add files via upload

--- P02_06.py
+++ P02_06.py
@@ -1,6 +1,12 @@
 def sortId(L):
     # ADD ADDITIONAL CODE HERE!
-    
+    A = []
+    L = sorted(list(zip(L[::2],L[1::2])))
+    for i in L:
+        for j in i:
+            A.append(j)
+    return A
+
 
 print sortId(["14-002","Kim","13-009","Lee","16-005","Na","15-003","Kim"])
-#[’13-009’,’Lee’,’14-002’,’Kim’,’15-003’,’Kim’,’16-005’,’Na’]
+#['13-009','Lee','14-002','Kim','15_003','Kim','16-005','Na']
